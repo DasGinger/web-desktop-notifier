93467fe8de38921444647e6039c2651c138cec02
jweber@demandjump.com <jweber@demandjump.com>
2020-12-05 23:49:39 -0500

    initial commit

---
+++ web-notifiers/playstation-direct-notifier.js
@@ -0,0 +1,78 @@
+// Install an auto refresh add-on for your browser that allows you to execute Javascript code when refreshed
+// Firefox: https://addons.mozilla.org/en-US/firefox/addon/tab-reloader/
+// Chrome: https://chrome.google.com/webstore/detail/tab-reloader-page-auto-re/dejobinhdiimklegodgbmbifijpppopn?hl=en
+//
+// MAKE SURE TO ALLOW DESKTOP NOTIFICATIONS FROM THE PLAYSTATION DIRECT TAB
+
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  };
+
+// Booleans
+var isInStock = false;
+var queueIsLive = false;
+
+// constant variables
+var productTitle = document.querySelector('.sony-text-h1').textContent;
+var productImg = document.querySelector('.productContainer hero-image picture .hero-banner').attributes['src'].value;
+var notificationTitle = '';
+var n_options = {
+    body: `${productTitle} is now in stock!\nClick this notification to view this product now.`,
+    silent: false,
+    icon: productImg,
+    tag: `playstation_direct:${productTitle}`,
+    renotify: true,
+    requireInteraction: true,
+    vibrate: true
+};
+
+// HTML elements on page
+var addToCartBtn = document.querySelector('[aria-label="Add to Cart"]');
+var outOfStockMsg = document.querySelector('.button-placeholder .out-stock-wrpr .sony-text-body-1');
+var siteTitle = document.querySelector('head title');
+
+try {
+    if(!siteTitle.textContent.toLowerCase().includes('queue')){
+        if(outOfStockMsg.parentElement.classList.contains('hide')){
+            if(addToCartBtn !== null){
+                isInStock = true;
+                notificationTitle = `${productTitle} is in stock!`;
+            } else {
+                throw 'No valid HTML elements are present on the webpage. Make sure you are on a product page on target.com';
+            }
+        } else {
+            // product is out of stock
+            console.log(`${productTitle} is out of stock. Skipping notification...`);
+        }
+    } else {
+        queueIsLive = true;
+        notificationTitle = 'Playstation Direct queue has started!';
+        n_options.body = 'Click this notification to go to Playstation Direct and take your place in line. Good luck!';
+    }
+} catch(err) {
+    console.log(err);
+}
+
+if(isInStock || queueIsLive){
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    else if (Notification.permission === "granted") {
+        var notification = new Notification(notificationTitle, n_options);
+    }
+
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
+            var notification = new Notification(notificationTitle, n_options);
+          }
+        });
+      }
+}
---
+++ web-notifiers/target-notifier.js
@@ -0,0 +1,89 @@
+// Install an auto refresh add-on for your browser that allows you to execute Javascript code when refreshed
+// Firefox: https://addons.mozilla.org/en-US/firefox/addon/tab-reloader/
+// Chrome: https://chrome.google.com/webstore/detail/tab-reloader-page-auto-re/dejobinhdiimklegodgbmbifijpppopn?hl=en
+//
+// MAKE SURE TO ALLOW DESKTOP NOTIFICATIONS FROM THE TARGET TAB
+
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  };
+
+window.addEventListener('load', function(){
+    // Booleans
+    var isInStock = false;
+    var availabilityType = '';
+    
+    // constant variables
+    var productTitle = document.querySelector('[data-test="product-title"]').textContent;
+    var productImg = document.querySelector('.slide--active a .slideDeckPicture div div div img').attributes['src'].value;
+
+    // HTML elements on page
+    pickItUpBtn = document.querySelector('[data-test="orderPickupButton"]');
+    deliverItBtn = document.querySelector('[data-test="scheduledDeliveryButton"]');
+    shipItBtn = document.querySelector('[data-test="shipItButton"]');
+    outOfStockMsg = document.querySelector('[data-test="outOfStockMessage"]');
+
+    // Check each button for a class named 'hide' denoting that it is hidden and unavailable
+    // There is a heirachy of priority - realized by the below logic statement
+    try{
+        if(outOfStockMsg == null){
+            // wait for elements to load - target's website is slow :)
+            while(shipItBtn == null || deliverItBtn == null || pickItUpBtn == null) {
+                continue;
+            }
+            if(!shipItBtn.classList.contains('hide')){
+                availabilityType = 'shipping';
+                isInStock = true;
+            } else if (!deliverItBtn.classList.contains('hide')) {
+                availabilityType = 'delivery';
+                isInStock = true;
+            } else if (!pickItUpBtn.classList.contains('hide')) {
+                availabilityType = 'pick up';
+                isInStock = true;
+            } else {
+                throw 'No valid HTML elements are present on the webpage. Make sure you are on a product page on target.com';
+            }
+        } else {
+            // product is out of stock
+            console.log(`${productTitle} is out of stock. Skipping notification...`);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+
+
+    var notificationTitle = `${productTitle} :: ${toTitleCase(availabilityType)}`;
+    var n_options = {
+        body: `${productTitle} is now in stock and available via ${availabilityType}!\nClick this notification to view this product now.`,
+        silent: false,
+        icon: productImg,
+        tag: `target:${productTitle}`,
+        renotify: true,
+        requireInteraction: true,
+        vibrate: true
+    };
+
+    if(isInStock){
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+        }
+    
+        else if (Notification.permission === "granted") {
+            var notification = new Notification(notificationTitle, n_options);
+        }
+    
+        else if (Notification.permission !== 'denied' || Notification.permission === "default") {
+            Notification.requestPermission().then(function (permission) {
+              // If the user accepts, let's create a notification
+              if (permission === "granted") {
+                var notification = new Notification(notificationTitle, n_options);
+              }
+            });
+        }
+    }
+});
